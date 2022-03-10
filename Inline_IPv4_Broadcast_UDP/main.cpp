cff653e47080d64514af5aa477665fdd8412376c
Rick Armbruster <rick.armbruster.gb@gmail.com>
2022-03-10 19:03:27 +0100

    init

---
+++ Inline_IPv4_Broadcast_UDP/main.cpp
@@ -0,0 +1,7 @@
+#include"Inline_IPv4_Broadcast_UDP.h"
+int main()
+{
+    Inline_IPv4_Broadcast_UDP init;
+    init.Broadcast();
+}
+
---
+++ Inline_IPv4_Receiver/main.cpp
@@ -0,0 +1,6 @@
+#include"Inline_IPv4_Receiver.h"
+int main()
+{
+    Inline_IPv4_Receiver Init;
+    Init.Receive();
+}
